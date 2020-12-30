fbc8cfc11a742523f379caa1b11dd572e219d45d
jmp3833 <jmpeterson132@gmail.com>
2020-12-30 14:53:08 -0500

    Initial setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to quicklitics_client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/quicklitics_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns quicklitics-client.core-test
+  (:require [clojure.test :refer :all]
+            [quicklitics-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
